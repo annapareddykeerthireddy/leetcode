d640280b594c19473b1bc4c579d791922882bbc1
annapareddykeerthireddy <keerthi040204@gmail.com>
2025-05-19 14:15:08 +0530

    Time: 7 ms (38.26%), Space: 45.8 MB (60.77%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -4,13 +4,13 @@ class Solution {
         int n = img[0].length;
         int[][] result = new int[m][n];
 
-        // Traverse each pixel
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int sum = 0;
                 int count = 0;
 
-                // Check all 3x3 surrounding cells
+             
                 for (int r = i - 1; r <= i + 1; r++) {
                     for (int c = j - 1; c <= j + 1; c++) {
                         if (r >= 0 && r < m && c >= 0 && c < n) {
@@ -27,4 +27,3 @@ class Solution {
         return result;
     }
 }
-
